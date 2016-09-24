2ccb6eee0b2e9648ac6113a99f59f93759519c0f
Mikechaos <mikechaos13@gmail.com>
2016-09-24 05:37:14 -0500

    Refactor exercise 5
    Add comments to the parser, define a few more functions
    and define a macro to simplify testing for a nil value.
    I believe that it is probably something that already exist

--- ex-5.clj
+++ ex-5.clj
@@ -1,13 +1,23 @@
+
 (def program ["P 2 # select pen 2"
-            "D # pen down"
-            "W 2 # draw west 2cm"
-            "N 1 # then north 1"
-            "E 2 # then east 2"
-            "S 1 # then back south"
-            "U # pen up"])
+              "D # pen down"
+              "W 2 # draw west 2cm"
+              "N 1 # then north 1"
+              "E 2 # then east 2"
+              "S 1 # then back south"
+              "U # pen up"])
+
+(def commands
+  { :D "Pen Down"
+    :E "Draw East %cm"
+    :N "Draw North %cm"
+    :P "Select Pen %"
+    :U "Pen Up"
+    :S "Draw South %cm"
+    :W "Draw West %cm" })
 
 
-#_ "Complex way of doing exactly (map first program)"
+#_(Complex way of doing exactly (map first program))
 (def extractCommand
   (fn [program]
     (loop [[cmd & rst] program acc []]
@@ -17,30 +27,45 @@
         (recur rst (concat acc [(first cmd)]))))))
 
 
-#_ "String manipulations"
-(def replace clojure.string/replace)
-(defn safe-replace [v p r]
-  (if (not (nil? v)) (replace v p r) ""))
-(defn trim [v]
-  (safe-replace v #" " ""))
+#_(Flow control Macros)
+(defmacro test-nil 
+  "Simplifies testing if a value is nil
+  Usage : (test-nil value body-then body-else)
+  Expands: (if (not (nil? value)) body-then body-else)
+  Example : (test-nil 1 :good-job :feelsbadman) ==> :good-job"
+  [value body-then body-else] `(if (not (nil? ~value)) ~body-then ~body-else))
 
-#_ "Parse the program into tokens"
+
+
+#_(String manipulations)
+(def replace clojure.string/replace)
+(defn safe-replace [v p r] (test-nil v (replace v p r) ""))
+(defn trim [v] (safe-replace v #" " ""))
+
+#_(Parse the program into tokens)
+(defn match-coll [pattern input]
+ (map first (map #(re-seq pattern %) input)))
+
 (defn parser [program]
   (map (fn [[_ c m & rst]] {:cmd c :mod (trim m) :comment rst})
-       (map first
-            (map #(re-seq #"([A-Z])(\s[0-9])?(\s#.*)?$" %) program))))
+   (match-coll #"
+      ([A-Z]+)   # Command grouping
+      (\s[0-9])? # Modifier grouping
+      (\s#.*)?$  # Comment grouping"
+    program)))
 
-(def commands
-  { :P "Select Pen %"
-    :D "Pen Down"
-    :N "Draw North %cm"
-    :E "Draw East %cm"
-    :S "Draw South %cm"
-    :W "Draw West %cm"
-    :U "Pen Up" })
 
+#_(Token Accessors)
+(def get-modifier :mod)
+
+#_(To access the hashmap, we need the :keyword form of the command)
+(defn keyword-command [token] (keyword (:cmd token)))
+(defn get-command [token] (get commands (keyword-command token)))
+
+#_(In this simple example, applying a command means replacing it's
+   potential parameter with the corresponding modifier token)
+(defn apply-command [token] (replace (get-command token) #"%" (get-modifier token)))
 
 (def tokens (parser program))
-(map #(replace (get commands (keyword (:cmd %))) #"%" (:mod %)) tokens)
-
+(map #(apply-command %) tokens)
 #_(Evaluates to => ("Select Pen 2" "Pen Down" "Draw West 2cm" "Draw North 1cm" "Draw East 2cm" "Draw South 1cm" "Pen Up"))
