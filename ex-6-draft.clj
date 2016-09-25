5d5ac23ba5557ea9194c342955c74c3080b4c095
Mikechaos <mikechaos13@gmail.com>
2016-09-25 02:35:17 -0400

    Ex-6 - Define BNF grammar for time inputs
    The goal is to implement a simple program that will create the grammar
    And verify inputs against it

---
+++ ex-6-draft.clj
@@ -0,0 +1,17 @@
+(def possible-formats ["4pm" "7:38pm" "23:42" "3:16" "3:16am"])
+
+; Grammar
+;
+; <time>                 ::= <12-hour><meridiem-indicator> | <12-hour><separator><minute><meridiem-indicator> | <24-hour><separator><minute> | <12-hour><separator><minute>
+; <12-hour>              ::= <digit> | "10" | "11" | "12"
+; <24-hour>              ::= <first-digit-hour><digit> | "2"<second-digit-hour>
+; <minute>               ::= <first-digit-minute><second-digit-minute> | ""
+; <digit>                ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+; <first-digit-hour>     ::= "0" | "1"
+; <second-digit-hour>    ::= "0" | "1" | "2" | "3" | "4"
+; <first-digit-minute>   ::= "0" | "1" | "2" | "3" | "4" | "5"
+; <second-digit-minute>  ::= <digit>
+; <separator>            ::= ":" | ""
+; <meridiem-indicator>   ::= "am" | "pm"
+;
+
